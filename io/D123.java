23da956e6262198033c6779cb30790e23a21775b
sl <875834446@qq.com>
2017-12-16 00:18:30 +0800

    123D

---
+++ io/D123.java
@@ -0,0 +1,17 @@
+package io;
+
+/**
+ *
+ * @Author sl
+ * @Date 2017年12月16日 上午12:17:19
+ * @Version 1.0
+ * 
+ */
+public class D123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
